6fb57686c251b155e726c2579175ffee0d4ad6de
Yermek <ayerabdi@gmail.com>
2022-11-28 23:56:39 +0600

    setup integration

--- main.js
+++ main.js
@@ -1,4 +1,37 @@
-const path = require('path')
+// const path = require('path')
+// const {app, BrowserWindow, Menu} = require('electron')
+
+// const menu = [
+//     {
+//         label:'Меню',
+//         submenu: [
+//             {
+//                 label: 'Выйти',
+//                 click: () => app.quit(),
+//                 accelerator : 'Ctrl+k'
+//             }
+//         ]
+//     }
+// ]
+
+// const isDev = process.env.NODE_ENV !== 'development'
+
+// function createMainWindow(){
+//     const mainWindow = new BrowserWindow({
+//         title: 'VESOVAYA',
+//         width: isDev ? 700 : 800,
+//         heigth: 600
+//     });
+
+//     if(isDev) {
+//         mainWindow.webContents.openDevTools()
+//     }
+
+//     //mainWindow.loadFile(path.join(__dirname, './renderer/index.html'));
+//     mainWindow.loadURL("http://localhost:3000/")
+// }
+
+
 const {app, BrowserWindow, Menu} = require('electron')
 
 const menu = [
@@ -14,25 +47,34 @@ const menu = [
     }
 ]
 
-const isDev = process.env.NODE_ENV !== 'development'
 
-function createMainWindow(){
-    const mainWindow = new BrowserWindow({
-        title: 'VESOVAYA',
-        width: isDev ? 1500 : 800,
-        heigth: 600
+async function createMainWindow () {
+    const win = new BrowserWindow({
+      width: 1000,
+      height: 800,
+      webPreferences: {
+        //enableRemoteMode: true,
+        nodeIntegration: true,
+        devTools: false,
+      },
     });
+   
+    await win.loadURL('http://localhost:3000');
+  
+  //set up the express server into the same main.js file //
+  const expApp = require("express");
+  const path = require("path");
+  
+  expApp.use(express.static(path.join(__dirname, "./src/build")));
+  expApp.get("/", function (req, res) {
+    res.send(path.join(__dirname, "./src/build", "index.html"));
+  });
+  
+  expApp.listen(3000, () => {
+    console.log("Server runing at Port: 3001");
+  })}
 
-    if(isDev) {
-        mainWindow.webContents.openDevTools()
-    }
-
-    mainWindow.loadFile(path.join(__dirname, './renderer/index.html'));
-}
-
-
-
-app.whenReady().then( () => {
+  app.whenReady().then( () => {
 
     const mainMenu = Menu.buildFromTemplate(menu)
     Menu.setApplicationMenu(mainMenu)
